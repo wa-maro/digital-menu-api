bbbf1b5d9ff6d399b96f858b9fcb8ff5727ac5df
Mathew Maro <theo.pombe@gmail.com>
2025-05-24 02:34:37 +0000

    add permission to orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -14,31 +14,37 @@ import { AuthGuard } from '@nestjs/passport';
 import { CustomRequest } from 'src/interfaces/custom-request.interface';
 import { RoleGuard } from 'src/common/guards/roles.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
+import { PermissionsGuard } from 'src/common/guards/permissions.guard';
+import { Permissions } from 'src/common/decorators/permissions.decorator';
 
-@UseGuards(AuthGuard('jwt'), RoleGuard)
+@UseGuards(AuthGuard('jwt'), RoleGuard, PermissionsGuard)
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Roles('admin', 'manager', 'customer')
+  @Permissions('order:place')
   @Post()
   placeOrder(@Body() dto: PlaceOrderDto, @Req() req: CustomRequest) {
     return this.ordersService.placeOrder(req.user['userId'], dto);
   }
 
   @Roles('customer')
+  @Permissions('order:read:own')
   @Get()
   getMyOrders(@Req() req: CustomRequest) {
     return this.ordersService.getUserOrders(req.user['userId']);
   }
 
   @Roles('admin', 'manager', 'customer')
+  @Permissions('order:read')
   @Get(':id')
   getOrder(@Param('id') id: string, @Req() req: CustomRequest) {
     return this.ordersService.getOrderById(id, req.user['userId']);
   }
 
   @Roles('manager', 'admin')
+  @Permissions('order:update:status')
   @Put(':id/status')
   updateOrderStatus(@Param('id') id: string, @Body() dto, @Req() req: Request) {
     return this.ordersService.updateStatus(id, dto.status);
@@ -46,6 +52,7 @@ export class OrdersController {
 
   @Get('all')
   @Roles('manager', 'admin')
+  @Permissions('order:read:all')
   getAllOrders() {
     return this.ordersService.getAllOrders();
   }
