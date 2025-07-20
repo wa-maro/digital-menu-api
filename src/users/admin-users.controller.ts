065bbd9418cd8c54d8a384705a41b1587089bf71
theomaro <theo.pombe@gmail.com>
2025-07-20 16:39:56 +0300

    manage admin profile

--- src/users/admin-users.controller.ts
+++ src/users/admin-users.controller.ts
@@ -1,16 +1,33 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { RoleGuard } from 'src/common/guards/roles.guard';
 import { UsersService } from './users.service';
+import { CustomRequest } from 'src/interfaces/custom-request.interface';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @UseGuards(AuthGuard('jwt'), RoleGuard)
 @Roles('admin', 'manager')
-@Controller('admin/customers')
+@Controller('admin')
 export class AdminUsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
+  @Get('me/profile')
+  async getProfile(@Req() req: CustomRequest) {
+    const userId = req.user.userId;
+    return await this.usersService.findUserWithProfile(userId);
+  }
+
+  @Patch('me/profile')
+  async updateProfile(
+    @Req() req: CustomRequest,
+    @Body() dto: UpdateProfileDto,
+  ) {
+    const userId = req.user.userId;
+    return await this.usersService.updateOrCreateProfile(userId, dto);
+  }
+
+  @Get('customers')
   async getCustomers() {
     return this.usersService.findCustomers();
   }
